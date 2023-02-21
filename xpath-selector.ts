3197312ed53000157c731b667497d32640c8e458
hainhp-diprovn <hainhp@dipro.vn>
2023-02-21 11:50:55 +0700

    Sample

---
+++ xpath-selector.ts
@@ -0,0 +1,17 @@
+import { Selector } from 'testcafe';
+
+const XPathSelector = Selector(xpath => {
+    const iterator = document.evaluate(xpath, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    const items = [];
+
+    let item = iterator.iterateNext();
+
+    while (item) {
+        items.push(item);
+        item = iterator.iterateNext();
+    }
+
+    return items;
+});
+export default XPathSelector
+// export default XPathSelector =  Selector(getElementsByXPath(xpath))
