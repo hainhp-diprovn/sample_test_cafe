d1f2165299977fdd670bbc0296d3d306d25a057c
hainhp-diprovn <hainhp@dipro.vn>
2023-04-11 15:56:04 +0700

    Setup config

--- .testcaferc.js
+++ .testcaferc.js
@@ -1,15 +1,32 @@
 module.exports = {
   src: "src/tests",
   baseUrl: "http://dipro.internal-dev.dipro-tech.com",
-  // "speed": 0.8
+  browsers: ["chrome"],
+  speed: 1,
   skipJsErrors: true,
+  skipUncaughtErrors: true,
   screenshots: {
     path: "screenshots/",
     takeOnFails: true,
     thumbnails: false,
     fullPage: true,
   },
+  filter: {
+    // fixtureGrep: "Page.*",
+    // fixture: "The signin page",
+    // test: "Click a label",
+    // testGrep: "Example *",
+    // testMeta: {
+    //   device: "mobile",
+    //   env: "production",
+    // },
+  },
   debugMode: false,
+  debugOnFail: false,
+  quarantineMode: {
+    successThreshold: 1,
+    attemptLimit: 3,
+  },
   concurrency: 1,
   hostname: "localhost",
   qrCode: true,
